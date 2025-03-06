0fd726832b070aed12faa24dec9c6913c69de8b7
Yohan D Osorio L <yooslzn1487@gmail.com>
2025-03-05 22:25:14 -0500

    Update layout component to ensure minimum height and adjust padding for better layout consistency

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -3,7 +3,7 @@
 function Layout ({ children }) {
   return (
     <>
-      <div className='flex flex-col items-center bg-[#272b33] mt-[50px]'>
+      <div className='flex flex-col items-center min-h-screen bg-[#272b33] pt-[80px]'>
         {children}
       </div>
     </>
