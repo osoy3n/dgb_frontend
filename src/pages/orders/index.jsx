4f396329ab558ef063ad44b5ecf4f2ce7fba83ac
Yohan D Osorio L <yooslzn1487@gmail.com>
2025-03-07 17:10:51 -0500

    Implement delete functionality for purchases and update order totals in OrdenesHechas component

--- src/pages/orders/index.jsx
+++ src/pages/orders/index.jsx
@@ -1,22 +1,57 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useContext } from 'react'
 import { contexto, axiosInstance } from '../../context'
 import Layout from '../../components/layout'
 import TablaCompras from '../../components/table'
+import { procesarOrdenes } from '../../utils/process-orders'
 
 function OrdenesHechas() {
   const { personajesComprados, setPersonajesComprados } = useContext(contexto)
 
   useEffect(() => {
+    cargarCompras()
+  }, [])
+    
+  const cargarCompras = () => {
     axiosInstance.get('/lista_compras')
-      .then(respuesta => setPersonajesComprados(respuesta.data))
-  }, [setPersonajesComprados])
+    .then(respuesta => setPersonajesComprados(respuesta.data))
+  }
 
   const editar = (idCompra) => {
     console.log(idCompra)
   }
 
-  const eliminar = (idCompra) => {
-    console.log(idCompra)
+  const eliminar = async (idCompra, idOrden) => {
+    try {
+      await axiosInstance.delete(`/compras/${idCompra}`)
+
+      const nuevasCompras = personajesComprados.filter(
+        compra => compra.id_compra !== idCompra
+      )
+
+      setPersonajesComprados(nuevasCompras)
+
+      const ordenesActualizadas = await procesarOrdenes(nuevasCompras)
+      const ordenActualizada = ordenesActualizadas.find(
+        orden => orden.id_orden === idOrden
+      )
+
+      if (!ordenActualizada) {
+        await axiosInstance.delete(`/ordenes/${idOrden}`)
+        return
+      }
+
+      const usuario = JSON.parse(localStorage.getItem('usuarioAuth'))
+      await axiosInstance.patch(`/ordenes/${idOrden}`, {
+        total_item: ordenActualizada.total_item,
+        total_precio: ordenActualizada.total_precio,
+        id_usuario: usuario.id
+      })
+      cargarCompras()
+    } catch (error) {
+      console.error('Error en la operación:', error)
+      cargarCompras()
+    }
   }
 
   return (
